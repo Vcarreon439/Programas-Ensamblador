0e0b0aa992aa12f9db04c96b3e03ce2c5a1dfcf0
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2022-03-30 20:42:51 -0600

    .
    .

---
+++ Exponente con Saltos.asm
@@ -0,0 +1,111 @@
+Macro Solicitar_Num mensaje
+    
+    MOV DX, offset mensaje
+    MOV AH, 09H
+    INT 21H
+    
+    MOV AH,01H
+    INT 21H
+    
+ENDM
+
+.model small
+.stack 32
+.data
+
+    msm db "Ingrese la base: $"
+    msm2 db 10,13, "Ingrese la potencia: $"
+    ms2 db 10,13, "El resultado es: $"
+    valor db ?
+    str db 6 dup('$')
+    
+.code
+   
+mov ax,@data    ;Segmento de datos
+mov ds,ax
+
+Solicitar_Num msm
+MOV AH, 00
+SUB AL, 30H
+MOV BX, AX      ;Guardamos en BX la base
+
+Solicitar_Num msm2
+MOV AH, 00
+SUB AL, 30H
+MOV CX, AX      ;Guardamos en CX la potencia
+
+MOV AX,1        ;Reestablecer el valor de ax para comenzar el factorial
+
+CicloFactorial:
+    CMP CL, 0
+    JE  Conversion
+    JA  Factorial 
+ 
+Factorial:
+
+    MUL BX
+    DEC CL
+    JMP CicloFactorial
+                            
+            
+;Para el calculo de la potencia, BX se multiplica con AX
+
+
+Conversion:
+
+    call numeroAcadena  ;Invocacion de convertir digito en cadena
+    
+    ;Mensaje y salto de linea para mejor escritura
+      mov  dx, offset ms2
+      mov  ah, 09h
+      int  21h
+    
+    ;Impresion en pantalla
+      mov  dx, offset str   
+      mov  ah, 09h
+      int  21h
+
+Finalizar:
+
+;Esperamos entrada del usuario
+  mov  ah,7
+  int  21h
+
+;Finalizamos el programa.
+  mov  ax, 4c00h
+  int  21h           
+
+;------------------------------------------
+
+;El valor de la cadena debe estar en AX
+;sayinin digitlerini teker teker cikarir
+;Resta los digitos del numero, uno por uno
+;Elimina los numeros de la pila y los inserta en la matriz
+
+proc numeroAcadena
+                        
+  mov  bx, 10   ;Los numeros se restan dividiendo por 10, el 10 asignado a BX
+  mov  cx, 0    ;El contrador esta configurado para los numeros restados
+  
+ciclo1:       
+  mov  dx, 0    ;El valor en DX se reestablece, y DX se usa como apuntador
+                ;El valor restante de esta seccion contendra el digito extraido
+  div  bx       ;El valor en AX se divide por BX (10)
+  push dx       ;DX se agrega a la pila de digitos
+  inc  cx       ;El contador 'CX' se incrementa en 1
+  cmp  ax, 0    ;El proceso continua hasta que AX = 0
+  jne  ciclo1
+
+  mov  si, offset str   ;Se asigna el valor a la cadena
+  
+ciclo2:  
+  pop  dx        
+  add  dl, 48   ;Los valores de la pila se eliminan uno por uno y se asignan a str
+  mov  [ si ], dl
+  inc  si
+  loop ciclo2
+
+  ret
+endp
+
+end
